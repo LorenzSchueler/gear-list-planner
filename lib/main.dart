57b5aa605d28cfe06ef4ad0cb77b61f74f7badeb
Lorenz Schüler <lorenz@saarow.de>
2023-08-15 16:04:21 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gear List Planner',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Scaffold(body: Text('Flutter Demo Home Page')),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
