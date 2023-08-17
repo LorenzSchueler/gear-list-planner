e54141b0712f8ec00bcff502e126f11e12726a9b
Lorenz Schüler <lorenz@saarow.de>
2023-08-17 11:13:20 +0200

    feat: make ids in model final, add Entity class

--- lib/model.dart
+++ lib/model.dart
@@ -3,52 +3,61 @@ import 'package:json_annotation/json_annotation.dart';
 part "model.g.dart";
 
 abstract class Id {
-  int get _id;
+  int get id;
 
-  static int _toJson(Id id) => id._id;
+  static int _toJson(Id id) => id.id;
+}
+
+abstract class Entity<I extends Id> {
+  I get id;
+
+  Map<String, dynamic> toJson();
 }
 
 class GearListId implements Id {
-  GearListId(this._id);
+  GearListId(this.id);
 
   @override
-  final int _id;
+  final int id;
 
   @override
-  bool operator ==(other) => other is GearListId && other._id == _id;
+  bool operator ==(other) => other is GearListId && other.id == id;
 
   @override
-  int get hashCode => _id.hashCode;
+  int get hashCode => id.hashCode;
 }
 
 @JsonSerializable()
-class GearList {
+class GearList extends Entity<GearListId> {
   GearList({required this.id, required this.name});
 
   factory GearList.fromJson(Map<String, dynamic> json) =>
       _$GearListFromJson(json);
 
+  @override
   Map<String, dynamic> toJson() => _$GearListToJson(this);
 
+  @override
   @JsonKey(fromJson: GearListId.new, toJson: Id._toJson)
   GearListId id;
   String name;
 }
 
 class GearListVersionId implements Id {
-  GearListVersionId(this._id);
-  @override
-  final int _id;
+  GearListVersionId(this.id);
 
   @override
-  bool operator ==(other) => other is GearListVersionId && other._id == _id;
+  final int id;
 
   @override
-  int get hashCode => _id.hashCode;
+  bool operator ==(other) => other is GearListVersionId && other.id == id;
+
+  @override
+  int get hashCode => id.hashCode;
 }
 
 @JsonSerializable()
-class GearListVersion {
+class GearListVersion extends Entity<GearListVersionId> {
   GearListVersion({
     required this.id,
     required this.gearListId,
@@ -59,10 +68,12 @@ class GearListVersion {
   factory GearListVersion.fromJson(Map<String, dynamic> json) =>
       _$GearListVersionFromJson(json);
 
+  @override
   Map<String, dynamic> toJson() => _$GearListVersionToJson(this);
 
+  @override
   @JsonKey(fromJson: GearListVersionId.new, toJson: Id._toJson)
-  GearListVersionId id;
+  final GearListVersionId id;
   @JsonKey(fromJson: GearListId.new, toJson: Id._toJson)
   GearListId gearListId;
   String name;
@@ -70,20 +81,20 @@ class GearListVersion {
 }
 
 class GearListItemId implements Id {
-  GearListItemId(this._id);
+  GearListItemId(this.id);
 
   @override
-  final int _id;
+  final int id;
 
   @override
-  bool operator ==(other) => other is GearListItemId && other._id == _id;
+  bool operator ==(other) => other is GearListItemId && other.id == id;
 
   @override
-  int get hashCode => _id.hashCode;
+  int get hashCode => id.hashCode;
 }
 
 @JsonSerializable()
-class GearListItem {
+class GearListItem extends Entity<GearListItemId> {
   GearListItem({
     required this.id,
     required this.gearItemId,
@@ -95,10 +106,12 @@ class GearListItem {
   factory GearListItem.fromJson(Map<String, dynamic> json) =>
       _$GearListItemFromJson(json);
 
+  @override
   Map<String, dynamic> toJson() => _$GearListItemToJson(this);
 
+  @override
   @JsonKey(fromJson: GearListItemId.new, toJson: Id._toJson)
-  GearListItemId id;
+  final GearListItemId id;
   @JsonKey(fromJson: GearItemId.new, toJson: Id._toJson)
   GearItemId gearItemId;
   @JsonKey(fromJson: GearListVersionId.new, toJson: Id._toJson)
@@ -108,20 +121,20 @@ class GearListItem {
 }
 
 class GearItemId implements Id {
-  GearItemId(this._id);
+  GearItemId(this.id);
 
   @override
-  final int _id;
+  final int id;
 
   @override
-  bool operator ==(other) => other is GearItemId && other._id == _id;
+  bool operator ==(other) => other is GearItemId && other.id == id;
 
   @override
-  int get hashCode => _id.hashCode;
+  int get hashCode => id.hashCode;
 }
 
 @JsonSerializable()
-class GearItem implements Comparable<GearItem> {
+class GearItem extends Entity<GearItemId> implements Comparable<GearItem> {
   GearItem({
     required this.id,
     required this.gearCategoryId,
@@ -133,10 +146,12 @@ class GearItem implements Comparable<GearItem> {
   factory GearItem.fromJson(Map<String, dynamic> json) =>
       _$GearItemFromJson(json);
 
+  @override
   Map<String, dynamic> toJson() => _$GearItemToJson(this);
 
+  @override
   @JsonKey(fromJson: GearItemId.new, toJson: Id._toJson)
-  GearItemId id;
+  final GearItemId id;
   @JsonKey(fromJson: GearCategoryId.new, toJson: Id._toJson)
   GearCategoryId gearCategoryId;
   String name;
@@ -148,29 +163,31 @@ class GearItem implements Comparable<GearItem> {
 }
 
 class GearCategoryId implements Id {
-  GearCategoryId(this._id);
+  GearCategoryId(this.id);
 
   @override
-  final int _id;
+  final int id;
 
   @override
-  bool operator ==(other) => other is GearCategoryId && other._id == _id;
+  bool operator ==(other) => other is GearCategoryId && other.id == id;
 
   @override
-  int get hashCode => _id.hashCode;
+  int get hashCode => id.hashCode;
 }
 
 @JsonSerializable()
-class GearCategory {
+class GearCategory extends Entity<GearCategoryId> {
   GearCategory({required this.id, required this.name});
 
   factory GearCategory.fromJson(Map<String, dynamic> json) =>
       _$GearCategoryFromJson(json);
 
+  @override
   Map<String, dynamic> toJson() => _$GearCategoryToJson(this);
 
+  @override
   @JsonKey(fromJson: GearCategoryId.new, toJson: Id._toJson)
-  GearCategoryId id;
+  final GearCategoryId id;
   String name;
 }
 
